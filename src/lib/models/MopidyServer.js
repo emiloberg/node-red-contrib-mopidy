92c6b06ed0791d1a794f3174a9805e62243e111c
Emil Öberg <emil.oberg@monator.com>
2015-10-29 18:39:50 +0100

    Fixed broken execution of mopidy command

--- src/lib/models/MopidyServer.js
+++ src/lib/models/MopidyServer.js
@@ -65,14 +65,15 @@ export default class MopidyServer {
 	 * @private
 	 */
 	static _executeFunctionByName(functionName, context /*, args */) {
-	    var args = [].slice.call(arguments).splice(2);
-	    var namespaces = functionName.split('.');
-	    var func = namespaces.pop();
-	    for(var i = 0; i < namespaces.length; i++) {
-	        context = context[namespaces[i]];
-	    }
-	    return context[func](args);
+		var args = [].slice.call(arguments).splice(2);
+		var namespaces = functionName.split('.');
+		var func = namespaces.pop();
+		for(var i = 0; i < namespaces.length; i++) {
+			context = context[namespaces[i]];
+		}
+		return context[func].apply(this, args);
 	}
+	
 
 	set mopidyApi(api) {
 		var newApi = {};
