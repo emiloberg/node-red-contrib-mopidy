7b7a3248e38879f1ea53d446b249046cdfa0466c
Emil Öberg <emil.oberg@monator.com>
2015-11-03 17:04:45 +0100

    mopidy-in: can now select what to listen for

--- src/mopidy-in.js
+++ src/mopidy-in.js
@@ -1,5 +1,6 @@
 import servers from './lib/models/servers';
 var objectPath = require('object-path');
+require('string.prototype.startswith');
 
 module.exports = function(RED) {
     'use strict';
@@ -11,6 +12,7 @@ module.exports = function(RED) {
         this.RED.nodes.createNode(this,n);
         this.name = n.name;
         this.server = n.server;
+        this.messageType = n.messagetype;
         this.serverNode = this.RED.nodes.getNode(n.server);
 		this.mopidyServer = {
 			readyState: false
@@ -25,9 +27,9 @@ module.exports = function(RED) {
 			port: this.serverNode.port
 		});
 
-		this.mopidyServer.mopidy.on((fullEventName, eventData) => {
-			if (fullEventName.substr(0, 6) === 'event:') {
-				eventData.event = fullEventName.substring(6, fullEventName.length);
+		this.mopidyServer.mopidy.on((fullEventName, eventData = {}) => {
+			if(fullEventName.startsWith(this.messageType) || this.messageType === 'all') {
+				eventData.event = fullEventName;
 				this.send({
 					payload: eventData,
 					serverName: this.serverNode.name,
